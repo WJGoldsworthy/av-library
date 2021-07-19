9f1f854be33b58424c48120de3e71251a7754919
William Goldsworthy <wjgoldsworthy@gmail.com>
2021-07-19 16:21:02 +0100

    Change the model to load to fix safari load issues

--- src/library/Landing.jsx
+++ src/library/Landing.jsx
@@ -66,14 +66,13 @@ function Landing() {
       "assets/shaders/blob.vert",
       "assets/shaders/blob.frag"
     );
-    manModel = p5.loadModel("assets/models/man.obj", true);
+    manModel = p5.loadModel("assets/models/man2.obj", true);
   };
 
   const setup = (p5, canvasParentRef) => {
     p5.createCanvas(window.innerWidth, window.innerHeight, p5.WEBGL).parent(
       canvasParentRef
     );
-
     amp = new p5.constructor.Amplitude();
     fft = new p5.constructor.FFT();
     p5.rotateX(p5.PI);
@@ -93,12 +92,10 @@ function Landing() {
       Math.ceil(lastBackgroundColor[1] + colorDifs[1] * alpha),
       Math.ceil(lastBackgroundColor[2] + colorDifs[2] * alpha)
     );
-
     fft.analyze();
     const volume = amp.getLevel();
     let freq = fft.getCentroid();
     freq *= 0.001;
-
     const mapF = p5.map(freq, 0, 1, 0, 20);
     const mapA = p5.map(volume, 0, 0.2, 0, 0.5);
     if (scaleModifiers[shaderPick] * scaleGrowth !== 1) {
@@ -121,7 +118,6 @@ function Landing() {
         scaleGrowth = 1;
       }
     }
-
     if (colorPick === 3) {
       if (xPos <= 200) {
         xPos += 1.5;
@@ -131,25 +127,19 @@ function Landing() {
         xPos -= 1.5;
       }
     }
-
     p5.translate(xPos * 1.5, yPos);
-
     if (shaderPick === 0) {
       manShaders.setUniform("uTime", p5.frameCount);
       manShaders.setUniform("uFrequency", mapF);
       manShaders.setUniform("uAmp", mapA);
       p5.shader(manShaders);
       p5.noStroke();
-
       let newRot = 0.005;
-
       p5.rotateY(p5.frameCount * newRot);
       p5.rotateZ(p5.frameCount * newRot);
       p5.rotateX(p5.frameCount * -newRot * 0.8);
-
       p5.model(manModel);
     }
-
     if (shaderPick === 1) {
       p5.shader(blobShaders);
       blobShaders.setUniform("uTime", p5.frameCount);
